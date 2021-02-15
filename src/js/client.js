b08a03b9e793b58d45a26662170702b1ee29a157
Vitalii <butaji9i54@gmail.com>
2021-02-15 16:51:12 +0300

    split text into lines

--- src/js/client.js
+++ src/js/client.js
@@ -1,8 +1,7 @@
 const axios = require("axios");
-const TEXT_API = "https://fish-text.ru/get?type=paragraph&format=json&number=4";
+const TEXT_API = "https://fish-text.ru/get?type=paragraph&format=json&number=1";
 
 export function getRandomText() {
-  const textArr = [];
   return axios.get(TEXT_API).then(data => {
     return data.data.text;
   });
--- src/js/index.js
+++ src/js/index.js
@@ -5,9 +5,30 @@ import { getRandomText } from "./client";
 
 function initApp() {
   getRandomText().then(data => {
-    console.log(data);
-    // renderTextForTyping(data.data.text);
+    renderText(splitIntoLines(data, 60));
   });
+}
+
+function renderText(text) {}
+
+function splitIntoLines(text, lineWidth) {
+  const result = [];
+  const words = text.split(" ");
+  console.log(text);
+  let intermediateSize = 0;
+  const line = [];
+
+  for (const word of words) {
+    intermediateSize += word.length + 1;
+    if (intermediateSize > lineWidth) {
+      intermediateSize = 0;
+      result.push(line.slice());
+      line.length = 0;
+    }
+    line.push(word);
+  }
+  result.push(line.slice());
+  return result;
 }
 
 initApp();
