8eecb7515b47a7d986b4d8e89a27e3cb8b523bf4
Vitaliy <butaji9i54@gmail.com>
2021-02-16 23:26:08 +0300

    input done

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,64 @@
 import "/src/assets/style.css";
 import "/src/assets/scss.scss";
 
+import { settings } from "./data";
+
 import { getRandomText } from "./client";
 
 function initApp() {
-  getRandomText().then(data => {
-    renderText(splitIntoLines(data, 60));
+  getRandomText()
+    .then(data => {
+      renderText(splitIntoLines(data, 50));
+    })
+    .then(() => {
+      startSession();
+    });
+  document.addEventListener("keydown", event => {
+    checkUserInput(event.key);
   });
 }
 
-function renderText(text) {}
+function renderText(text) {
+  let letterIndex = 0;
+  const textBox = document.querySelector(".text");
+
+  text.forEach(elem => {
+    const line = document.createElement("div");
+
+    line.classList.add("line");
+
+    elem.forEach(elem => {
+      const word = document.createElement("div");
+      const letters = elem.split("");
+      const space = document.createElement("div");
+      space.innerHTML = "&nbsp;";
+
+      word.classList.add("word");
+
+      letters.forEach(elem => {
+        const letter = document.createElement("div");
+
+        letter.classList.add("letter");
+        letter.setAttribute("data-id", `${letterIndex}`);
+
+        letter.innerHTML = elem;
+
+        word.appendChild(letter);
+        letterIndex++;
+      });
+      space.setAttribute("data-id", `${letterIndex}`);
+      letterIndex++;
+      word.appendChild(space);
+      line.appendChild(word);
+    });
+
+    textBox.appendChild(line);
+  });
+}
 
 function splitIntoLines(text, lineWidth) {
   const result = [];
   const words = text.split(" ");
-  console.log(text);
   let intermediateSize = 0;
   const line = [];
 
@@ -29,6 +73,47 @@ function splitIntoLines(text, lineWidth) {
   }
   result.push(line.slice());
   return result;
+}
+
+function startSession() {
+  document.querySelectorAll(".letter")[0].classList.add("active");
+}
+
+function checkUserInput(character) {
+  const validation = new RegExp(`^[?!,-;.а-яА-ЯёЁ" "0-9]+$`);
+  const currentLetter = document.querySelector(`[data-id="${settings.currentIndex}"]`);
+  if (!validation.test(character)) {
+    return;
+  }
+
+  if (character.charCodeAt() === 32 && currentLetter.innerHTML === "&nbsp;") {
+    moveCursor(currentLetter);
+    settings.currentIndex++;
+    return;
+  }
+
+  if (currentLetter.innerHTML === character) {
+    moveCursor(currentLetter);
+    settings.currentIndex += 1;
+  } else {
+    catchError(currentLetter);
+    settings.currentIndex += 1;
+  }
+}
+function moveCursor(letter) {
+  letter.classList.remove("active");
+  letter.classList.add("completed");
+  document
+    .querySelector(`[data-id="${settings.currentIndex + 1}"]`)
+    .classList.add("active");
+}
+
+function catchError(letter) {
+  letter.classList.remove("active");
+  letter.classList.add("error");
+  document
+    .querySelector(`[data-id="${settings.currentIndex + 1}"]`)
+    .classList.add("active");
 }
 
 initApp();
