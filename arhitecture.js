3d5cb4f714e9e3be9ac96e04519f8e5e8af12754
Vitaliy <butaji9i54@gmail.com>
2021-02-21 22:12:20 +0300

    arhitecture

---
+++ arhitecture.js
@@ -0,0 +1,33 @@
+class TextProvider {
+  constructor(URL) {
+    this.URL = URL;
+  }
+
+  provideText() {
+    return fetch(this.URL);
+  }
+}
+
+const text = new TextProvider("Нужный URL");
+
+class TextProcessor {
+  constructor(text) {
+    this.text = text;
+  }
+
+  proceedText() {
+    return "Текст нарезанный на строки и слова";
+  }
+}
+
+const splitedText = new TextProcessor(text.provideText);
+
+class TextRenderer {
+    constructor (splitedText) {
+        this.splitedText= splitedText
+    }
+
+    renderText () {
+        render splitedText
+    }
+}
